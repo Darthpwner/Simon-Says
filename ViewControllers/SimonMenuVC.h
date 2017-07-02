6d7ec147f6883617c09d3fd3392612cce8b656e3
Darthpwner <matthewallenlin@gmail.com>
2017-07-02 01:53:20 -0700

    Created SimonMenuVC

---
+++ ViewControllers/SimonMenuVC.h
@@ -0,0 +1,13 @@
+//
+//  SimonMenuVC.h
+//  Simon-Says
+//
+//  Created by Matthew Allen Lin on 7/2/17.
+//  Copyright © 2017 Matthew Allen Lin Software. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+@interface SimonMenuVC : WKInterfaceController
+
+@end
---
+++ ViewControllers/SimonMenuVC.m
@@ -0,0 +1,13 @@
+//
+//  SimonMenuVC.m
+//  Simon-Says
+//
+//  Created by Matthew Allen Lin on 7/2/17.
+//  Copyright © 2017 Matthew Allen Lin Software. All rights reserved.
+//
+
+#import "SimonMenuVC.h"
+
+@implementation SimonMenuVC
+
+@end
