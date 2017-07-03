113e6bf1f7a13eb77342b3ef2b9ca7c4c433125f
Darthpwner <matthewallenlin@gmail.com>
2017-07-03 15:56:11 -0700

    Work on SimonGameVC

---
+++ Simon-Says-WatchKit Extension/SimonGameVC.h
@@ -0,0 +1,13 @@
+//
+//  SimonGameVC.h
+//  Simon-Says
+//
+//  Created by Matthew Allen Lin on 7/3/17.
+//  Copyright © 2017 Matthew Allen Lin Software. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+@interface SimonGameVC : WKInterfaceController
+
+@end
---
+++ Simon-Says-WatchKit Extension/SimonGameVC.m
@@ -0,0 +1,33 @@
+//
+//  SimonGameVC.m
+//  Simon-Says
+//
+//  Created by Matthew Allen Lin on 7/3/17.
+//  Copyright © 2017 Matthew Allen Lin Software. All rights reserved.
+//
+
+#import "SimonGameVC.h"
+
+@interface SimonGameVC()
+
+@end
+
+@implementation SimonGameVC
+
+- (void) awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+    
+    // Configure interface objects here.
+}
+
+- (void) willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void) didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+@end
